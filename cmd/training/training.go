cf86b5efe5deb7fbae3c3e242ebf30daf61d32c5
Pathe SENE <pathe.sene@gmail.com>
2023-10-14 01:29:48 +0000

    feat: start testing playe auto take

--- cmd/training/training.go
+++ cmd/training/training.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 
-	"github.com/jedib0t/go-pretty/table"
 	"pathe.co/zinx/gametake"
 	"pathe.co/zinx/pkg/cards"
 	"pathe.co/zinx/pkg/game"
@@ -12,77 +12,39 @@ import (
 )
 
 func main() {
-	takes := make([]constrainedTake, 4)
 	for {
 		g := newSampleGame()
-		playerTakes(g, takes)
+		takes := playerTakes(g)
+		for _, take := range takes {
+			fmt.Println(take)
+		}
+		time.Sleep(10 * time.Second)
 	}
 }
 
-func playerTakes(g *game.Game, takes []constrainedTake) {
+func playerTakes(g *game.Game) (takes []constrainedTake) {
 	_ptk := []gametake.GameTake{}
 	for _, player := range g.GetPlayers() {
-		var takeID int = -1
 		player.PreetyHand()
-		for {
-			if g.GetTake() == gametake.TOUT {
-				fmt.Println("Going to start the game")
-				break
-			}
-
-			showTakeIfPasse(g)
-			showAvailableTakes(g)
-
-			fmt.Println("Auto player take")
-
-			t := table.NewWriter()
-			t.AppendHeader(table.Row{"TAKE", "All", "All Player", "CardsOfTake", "Player CardsOfTake", "Ratio", "Take Ratio", "CanTake"})
-			for key, tk := range player.GetTakes() {
-				t.AppendRow(tk.Print(key))
-			}
-			fmt.Println(t.Render())
-
-			fmt.Println("What is your take")
-			fmt.Scanf("%d", &takeID)
-			if takeID >= 0 && takeID < 7 {
-				fmt.Println("Your Take is: ", gametake.AllTakes[takeID].Name())
-				take := gametake.AllTakes[takeID]
-				g.AddTake(player.GetID(), take)
-
-				ctk := constrainedTake{take: take, takes: []gametake.GameTake{}, cards: player.Hand.Cards}
-				for _, t := range _ptk {
-					ctk.takes = append(ctk.takes, t)
-				}
-				_ptk = append(_ptk, take)
-				takes = append(takes, ctk)
-				fmt.Println(ctk.String())
-				break
-			}
+		if g.GetTake() == gametake.TOUT {
+			fmt.Println("Going to start the game")
+			break
 		}
-	}
-}
 
-func showAvailableTakes(g *game.Game) {
-	var takesSB strings.Builder
-	fmt.Println("Available takes")
-	for _, take := range gametake.AllTakes {
-		if take.GreaterThan(g.GetTake()) || take == gametake.PASSE {
-			takesSB.WriteString(fmt.Sprintf("%s %d,", take.Name(), take.GetValue()))
+		g.AddTake(player.GetID(), player.GetBestTake())
+		ctk := constrainedTake{take: *player.Take, takes: []gametake.GameTake{}, cards: player.Hand.Cards}
+		for _, t := range _ptk {
+			ctk.takes = append(ctk.takes, t)
 		}
+		_ptk = append(_ptk, *player.Take)
+		takes = append(takes, ctk)
 	}
-	fmt.Println(takesSB.String())
-}
-
-func showTakeIfPasse(g *game.Game) {
-	if g.GetTake() != gametake.PASSE {
-		fmt.Println("Current Take: ", g.GetTake().Name())
-	}
+	return takes
 }
 
 func newSampleGame() *game.Game {
 	g := game.NewGame()
 
-	fmt.Println("-------------Starting a new game ------------------")
 	a := player.NewPlayer()
 	g.AddPlayer(a)
 	b := player.NewPlayer()
