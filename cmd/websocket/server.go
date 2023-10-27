3edf8ed598956ff491e363afb327dc242668f253
Pathe SENE <pathe.sene@gmail.com>
2023-10-27 15:05:08 +0000

    refactor: extract socket server to a file

--- cmd/websocket/server.go
+++ cmd/websocket/server.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
-	"strconv"
 
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
-	"pathe.co/zinx/gametake"
-	"pathe.co/zinx/pkg/game"
-	"pathe.co/zinx/pkg/player"
+	"pathe.co/zinx/znet"
 )
 
 func main() {
 	app := fiber.New()
-	g := game.NewGame()
+	s := znet.NewSocketHandler()
 
 	app.Use("/ws", func(c *fiber.Ctx) error {
 		if websocket.IsWebSocketUpgrade(c) {
@@ -24,55 +20,7 @@ func main() {
 		return fiber.ErrUpgradeRequired
 	})
 
-	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
-		if g.NombreJoueurs < 4 {
-			p := player.NewPlayer()
-			p.Conn = c
-			g.AddPlayer(p)
-			log.Println(p)
-
-			r := game.ReceiveTakeHandMsg(*p, gametake.AllTakes)
-			m, _ := json.Marshal(r)
-			if err := c.WriteMessage(1, m); err != nil {
-				log.Println("write:", err)
-			}
-		}
-		var (
-			mt  int
-			msg []byte
-			err error
-		)
-		for {
-			if mt, msg, err = c.ReadMessage(); err != nil {
-				log.Println("error reading message:", err)
-				break
-			}
-
-			log.Printf("recv: %s %d", msg, mt)
-			obj := map[string]string{}
-			_ = json.Unmarshal(msg, &obj)
-
-			id, _ := strconv.Atoi(obj["id"])
-			if id == 2 {
-				pid, _ := strconv.Atoi(obj["player_id"])
-				err := g.AddTake(pid, gametake.AllTakesByName[obj["gametake"]])
-				if err != nil {
-					log.Println(err.Error())
-				}
-				log.Println(g.GetTake().Name())
-
-				b := game.BroadcastPlayerTakeMsg(obj["gametake"], id, g.AvailableTakes())
-				for _, p := range g.GetPlayers() {
-					if p != nil {
-						m, _ := json.Marshal(b)
-						if err := p.Conn.WriteMessage(1, m); err != nil {
-							log.Println("write:", err)
-						}
-					}
-				}
-			}
-		}
-	}))
+	app.Get("/ws/:id", websocket.New(s.Handle))
 
 	log.Fatal(app.Listen(":7777"))
 }
---
+++ znet/server_socket.go
@@ -0,0 +1,98 @@
+package znet
+
+import (
+	"encoding/json"
+	"log"
+	"strconv"
+
+	"github.com/gofiber/contrib/websocket"
+	"pathe.co/zinx/gametake"
+	"pathe.co/zinx/pkg/cards"
+	"pathe.co/zinx/pkg/game"
+	"pathe.co/zinx/pkg/player"
+)
+
+type SocketHandler struct {
+	g *game.Game
+}
+
+func NewSocketHandler() SocketHandler {
+	return SocketHandler{g: game.NewGame()}
+}
+
+func (s *SocketHandler) StartPlayerRegistration(c *websocket.Conn) {
+	if s.g.NombreJoueurs < 4 {
+		p := player.NewPlayer()
+		p.Conn = c
+		s.g.AddPlayer(p)
+		log.Println(p)
+
+		r := game.ReceiveTakeHandMsg(*p, gametake.AllTakes)
+		m, _ := json.Marshal(r)
+		if err := c.WriteMessage(1, m); err != nil {
+			log.Println("write:", err)
+		}
+	}
+}
+
+func (s *SocketHandler) Handle(c *websocket.Conn) {
+	s.StartPlayerRegistration(c)
+
+	var (
+		mt  int
+		msg []byte
+		err error
+	)
+	for {
+		if mt, msg, err = c.ReadMessage(); err != nil {
+			log.Println("error reading message:", err)
+			break
+		}
+
+		log.Printf("recv: %s %d", msg, mt)
+		obj := map[string]string{}
+		_ = json.Unmarshal(msg, &obj)
+
+		id, _ := strconv.Atoi(obj["id"])
+		if id == 2 {
+			s.HandlePlayerTake(c, obj)
+		}
+
+		if id == 4 {
+			s.HandlePlayerCard(c, obj)
+		}
+	}
+}
+
+func (s *SocketHandler) HandlePlayerTake(c *websocket.Conn, obj map[string]string) {
+	id, _ := strconv.Atoi(obj["id"])
+	pid, _ := strconv.Atoi(obj["player_id"])
+	err := s.g.AddTake(pid, gametake.AllTakesByName[obj["gametake"]])
+	if err != nil {
+		log.Println(err.Error())
+	}
+	log.Println(s.g.GetTake().Name())
+
+	if !s.g.TakesFinished {
+		b := game.BroadcastPlayerTakeMsg(obj["gametake"], id, s.g.AvailableTakes())
+		for _, p := range s.g.GetPlayers() {
+			if p != nil {
+				m, _ := json.Marshal(b)
+				if err := p.Conn.WriteMessage(1, m); err != nil {
+					log.Println("write:", err)
+				}
+			}
+		}
+	}
+}
+
+func (s *SocketHandler) HandlePlayerCard(c *websocket.Conn, obj map[string]string) {
+	pid, _ := strconv.Atoi(obj["player_id"])
+	c := cards.Card{Couleur: obj["couleur"], Genre: obj["genre"]}
+
+	err := s.g.PlayCard(pid, c)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
