ddb309993333fe9b0b6d34c7ddeacc0fb0264249
Pathe SENE <pathe.sene@gmail.com>
2023-11-05 20:06:43 +0000

    ci: setup playwright server

--- baloot/tests/app.spec.js
+++ baloot/tests/app.spec.js
@@ -4,13 +4,13 @@ const { test, expect } = require('@playwright/test');
 test('game play', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
-  const passe = await page.getByRole('button', { name: 'Passe' });
-  const trefle = await page.getByRole('button', { name: 'Trefle' });
+  const passe   = await page.getByRole('button', { name: 'Passe' });
+  const trefle  = await page.getByRole('button', { name: 'Trefle' });
   const carreau = await page.getByRole('button', { name: 'Carreau' });
-  const coeur = await page.getByRole('button', { name: 'Coeur' });
-  const pique = await page.getByRole('button', { name: 'Pique' });
-  const cent = await page.getByRole('button', { name: 'Cent' });
-  const tout = await page.getByRole('button', { name: 'Tout' });
+  const coeur   = await page.getByRole('button', { name: 'Coeur' });
+  const pique   = await page.getByRole('button', { name: 'Pique' });
+  const cent    = await page.getByRole('button', { name: 'Cent' });
+  const tout    = await page.getByRole('button', { name: 'Tout' });
 
   await expect(passe).toHaveText(/Passe/);
   await expect(trefle).toHaveText(/Trefle/);
