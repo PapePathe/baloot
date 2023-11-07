870c7b2b1a81ff8f02c356ad5ffa204b1978bc2b
Pathe SENE <pathe.sene@gmail.com>
2023-11-07 10:54:48 +0000

    use socket app

--- cmd/websocket/server.go
+++ cmd/websocket/server.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"log"
-
-	"github.com/gofiber/contrib/websocket"
-	"github.com/gofiber/fiber/v2"
 	"pathe.co/zinx/znet"
 )
 
 func main() {
-	app := fiber.New()
 	s := znet.NewSocketHandler()
+	app := znet.NewSocketApp("7777")
 
-	app.Use("/ws", func(c *fiber.Ctx) error {
-		if websocket.IsWebSocketUpgrade(c) {
-			c.Locals("allowed", true)
-
-			return c.Next()
-		}
-
-		return fiber.ErrUpgradeRequired
-	})
-
-	app.Get("/ws/:id", websocket.New(s.Handle))
-
-	log.Fatal(app.Listen(":7777"))
+	app.SetupRoutes(s)
+	app.Start()
 }
